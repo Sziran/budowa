0beb3859d7843e3fdb6d75b3164b1ac5f43ebd0f
Sziran <sziran2112@gmail.com>
2023-02-05 18:06:08 +0100

    .

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,12 @@
 const nav = document.querySelector('.mobile')
 const navBtn = document.querySelector('.burger')
 const allNavItems = document.querySelectorAll('.mobile__navi')
+const navBar = document.querySelector('.burger__bar')
 
 const handleNav = () => {
     nav.classList.toggle('mobile--active')
     navBtn.classList.toggle('burger--active')
+    navBar.classList.toggle('bar--active')
 
     allNavItems.forEach(item => {
         item.addEventListener('click', () => {
